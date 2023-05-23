c248828e368ef66456e82447a03cff141e0e1115
shubham <49603826+shubhdevelop@users.noreply.github.com>
2023-05-23 14:29:28 +0530

    Add basic functionality to the Game
    what changes did I made:
    - now user can make their choice
    - when user make choice computer also makes choice
    - on the basis of those choice we decide who wins and console log it the
    result.

---
+++ app/app.js
@@ -0,0 +1,97 @@
+let selection = document.querySelectorAll(".item");
+//defining current value of user
+let user;
+let computer;
+
+//Adding event listener to all the selection items
+selection.forEach((item)=> {
+console.log(item);
+item.addEventListener("click", getUserChoice);
+})
+
+
+//function to get the choice of the user  
+function getUserChoice(e){
+    user = e.target.alt;
+    console.log("user choice set ;", user);
+    //After setting userChoice set computer's choice
+    setComputerChoice();
+    start()
+}
+
+
+//this sets the computer choice to any random choice
+function setComputerChoice(){
+        computer = randomChoice()
+        console.log("computer choice set ;", computer);
+}
+
+
+
+//Make a random guess on behalf of the computer and returns
+// option out of any there possible value [rock, paper, scissors]
+function randomChoice() {
+    let randomChoice = Math.floor( Math.random() * 3 )
+    return ["rock", "paper", "scissors"][randomChoice]
+}
+
+//evokes the logic of the game after after both player have made their choice
+function start(){
+    console.log("both computer and Shubham has made a choice now");
+    decide(user, computer);
+}
+
+//Game logic here
+function decide(player1 , player2){
+
+    //based on the player1 choice of rock
+
+    if(player1 == "rock"){
+        switch (player2) {
+            case "paper":
+                console.log("user: You Lost!");
+                break;
+        
+            case "scissors":
+                console.log("user: you won");
+                break;
+            default:
+                console.log("This is a draw");
+        }
+    }
+
+    //based on the player1 choice of rock
+
+    if(player1 == "paper"){
+        switch (player2) {
+            case "scissors":
+                console.log("user: You Lost!");
+                break;
+        
+            case "rock":
+                console.log("user: you won");
+                break;
+            default:
+                console.log("This is a draw");
+        }
+    }
+
+    
+    //based on the player1 choice of rock
+
+    if(player1 == "scissors"){
+        switch (player2) {
+            case "rock":
+                console.log("user: You Lost!");
+                break;
+        
+            case "paper":
+                console.log("user: you won");
+                break;
+            default:
+                console.log("This is a draw");
+        }
+    }
+
+}
+
